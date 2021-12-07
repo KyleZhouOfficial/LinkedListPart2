794ea2acde207268d069b5c5ade860d450c35fb5
KZ <kyle.yiheng.zhou@gmail.com>
2021-12-07 12:17:29 -0800

    finished up print function and wrote comments

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,3 +1,11 @@
+/*
+Kyle Zhou
+This program creates a binary heap using an array and can read input from 
+a USER or FILE (num.txt). The print function uses a dfs to print out the 
+binary heap sideways. 
+12/7/21
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -7,10 +15,12 @@
 
 using namespace std;
 
+//prototypes
 void swim (int k, int heap[105]);
 void insert(int heap[105], int &N, int x);
 void sink(int N,int k, int heap[105]);
 
+//sink function if parent becomes less than child
 void sink(int N,int k, int heap[105]){
   //while not at end
   while(2*k <= N){
@@ -27,6 +37,7 @@ void sink(int N,int k, int heap[105]){
   }
 }
 
+//when child becomes greater than parent
 void swim (int k, int heap[105]){
   //while it is not the root and the parent is less than child
   while(k > 1 && heap[k/2] < heap[k]){
@@ -36,56 +47,96 @@ void swim (int k, int heap[105]){
   }
 }
 
+//insert function
 void insert(int heap[105], int &N, int x){
+  //add new number to latest index
   heap[++N] = x;
+  //move the child up to fix the tree
   swim(N, heap);
 }
 
+//remove
 void remove(int heap[105], int &N, char output[200000], int &spot){
+  //find max of binary heap 
   int max = heap[1];
+  //swap the top and bottom of the tree and decrease total number of nodes
   swap(heap[1], heap[N--]);
+  //sink the newly swapped element to restore balance
   sink(N, 1, heap);
+  //set the largest elemnt to 0
   heap[N+1] = 0;
-
-  int digits = 0;
-  int temp = max;
-  while(temp){
-    digits++;
-    temp /= 10;
-  }
-  for(int i = 0; i < digits; i++){
-    output[spot++] = max / pow(10, digits);
-  }
+  char space[1];
+  space[0] = ' ';
+  //char array concatenation found at: https://linuxhint.com/string-concatenation-in-c/
+  //add the max number to the output string
+  strcat(output, to_string(max).c_str());
+  strcat(output, space);
 }
 
-void print(int heap[105], int curr, bool visited[105]){
+//print function
+void print(int heap[105], int curr, bool visited[105], int N){
+  //if visited the current node or out of bounds return
   if(visited[curr] || curr > N) return;
+ 
   visited[curr] = true;
-
-  print(heap, curr * 2, visited);
-  print(heap, curr * 2 + 1, visited
+  //call function on left node
+  print(heap, curr * 2, visited, N);
+  //find what level the node is on 
+  int spaces = log2(curr);
+  //print out the corresponding spaces
+  for(int i = 0; i < 2*spaces; i++) cout << " ";
+  cout << heap[curr] << endl;
+  //call function on right node
+  print(heap, curr * 2 + 1, visited, N);
 }
 
 int main(){
+  //variables
   int heap[105];
   char output[200000];
   bool visited[105];
-  memset(visited, false, sizeof(visited);
+  memset(visited, false, sizeof(visited));
   int spot = 0;
   int N = 0;
+
+  //ask user for input type
   cout << "Enter USER for user input or FILE for file input?" << endl;
   char command[10];
   cin >> command;
   if(strcmp(command, "FILE") == 0){
+    //open file
     ifstream myfile("num.txt");
-    
-
-
+    //read until end of file
+    int num;
+    while(myfile >> num){
+      insert(heap, N, num);
+    }
     myfile.close();
   } else if(strcmp(command, "USER") == 0){
-    
+    cout << "Enter STOP to stop input" << endl;
+    char number[10];
+    //read 100 numbers or until stop
+    for(int i = 0; i < 100; i++){
+      cin >> number;
+	if(strcmp(number, "STOP")==0){ 
+	  break;
+	}
+	int temp = stoi(number);
+	insert(heap, N, temp); 
+      }
   }
 
-  
+  cout << "Tree Graph" << endl;
+   memset(visited, false, sizeof(visited));
+   //print out tree graph
+   print(heap, 1, visited, N);
+   cout << endl;
+   cout << "Removal of Numbers" << endl;
+   //remove numbers one by one and print the output
+   int toPrint = N;
+   for(int i = 0; i < toPrint; i++){
+     remove(heap, N, output, spot);
+     cout << output << endl;
+   }
   return 0;
 }
