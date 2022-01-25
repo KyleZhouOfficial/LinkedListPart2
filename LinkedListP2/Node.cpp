258d2dcc99f198fe1b64249e89634322f8e8c502
KZ <kyle.yiheng.zhou@gmail.com>
2022-01-25 11:29:12 -0800

    merged with kabir's code

--- LinkedListP2/Node.cpp
+++ LinkedListP2/Node.cpp
@@ -1,24 +1,58 @@
-#include "Node.h"
 #include "Student.h"
-
-Node::Node(Student* student) {
-    _student = student;
-    next = NULL;
-}
-
-Node::~Node() {
-    delete _student;
-    next = NULL;
-}
+#include "Node.h"
+#include <iostream>
+#include <cmath>
+#include <cstring>
+using namespace std;
 
 Student* Node::getStudent() {
-    return _student;
-}
-
-void Node::setNext(Node* newnext) {
-    next = newnext;
+  return val;
 }
 
 Node* Node::getNext() {
-    return next;
+  return next;
+}
+
+Node* Node::getLast() {
+  if (next==NULL) {
+      return this;
+    }
+  return next->getLast();
+}
+
+Node* Node::getCertain(int pos) {
+  if ((pos==0)||(next==NULL)) {
+    return this;
+  }
+  return next->getCertain(pos-1);
+}
+
+void Node::setNext(Node* n) {
+  next = n;
+}
+
+void Node::removeNext() {
+  next = NULL;
+}
+
+Node::Node() {
+  next = NULL;
+}
+
+Node::Node(Student* v) {
+  val = v;
+  next = NULL;
+}
+
+Node::Node(Node* n) {
+  next = n;
+}
+
+Node::Node(Student* v, Node* n) {
+  val = v;
+  next = n;
+}
+
+Node::~Node() {
+  delete val;
 }
--- LinkedListP2/Node.h
+++ LinkedListP2/Node.h
@@ -1,23 +1,29 @@
-/*
- * Node
- */
-#ifndef NODE_h
-#define NODE_h
-#include <iostream>
 #include "Student.h"
-using namespace std;
+
+#ifndef NODE_H
+#define NODE_H
 
 class Node {
-
-    public:
-        Node(Student* student); //create an instance of a Node
-        ~Node(); //destroy an instance of a Node
-        Student* getStudent(); //get the student stored in the Node
-        void setNext(Node* newnext); //set the next Node
-        Node* getNext(); //get the next Node
-
-    private:
-        Student* _student; //student
-        Node* next; //pointer to the next Node
+ public:
+  //these two are too simple, literally just give you the pointers to the corresponding values btw get certain is 0-indexing
+  Student* getStudent();
+  Node* getNext();
+  Node* getLast();
+  Node* getCertain(int pos);
+  //appends a Node to end of linked list
+  void setNext(Node* n);
+  //use this before destroying a Node if you don't want to destroy everything after the Node
+  void removeNext();
+  //initilizes Nodes with corresponding values
+  Node();
+  Node(Student* v);
+  Node(Node* n);
+  Node(Student* v, Node* n);
+  //destroys value
+  ~Node();
+ private:
+  Student* val;
+  Node* next;
 };
+
 #endif
